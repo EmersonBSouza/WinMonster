d011494352a87b47e835c12f76c85c0b0c311b66
MarcusAldrey <marcusaldrey@gmail.com>
2016-03-12 20:50:47 -0300

    Corrigido erro no método inserirComPrioridade(Object)

--- WINMonster/src/main/java/br/uefs/WinMonster/util/Fila.java
+++ WINMonster/src/main/java/br/uefs/WinMonster/util/Fila.java
@@ -4,65 +4,64 @@ public class Fila implements IFila{
 
 	//Celula primeiro;
 	private Lista lista = new Lista();
-	
+
 	@Override
 	public int obterTamanho(){
 		return lista.obterTamanho();
 	}
+
 	public void inserirComPrioridade(Object novoObjeto) {
 		Celula atual;
 		Celula anterior;
-		
+
 		if(lista.estaVazia()) {
 			Celula nova = new Celula();
+			nova.setObjeto(novoObjeto);
 			lista.primeiro = nova;
 		}
 		else {
 			atual = lista.primeiro;
 			anterior = lista.primeiro;
-
-			while(atual != null){
-				if( ((Comparable) novoObjeto).compareTo(atual.getObjeto()) > 0) {
-					anterior = atual;
-					atual = atual.getProximo();
-				}
-				if(atual.getObjeto() == lista.primeiro.getObjeto())
-					lista.inserirInicio(novoObjeto);
-				else {
-					Celula nova = new Celula();
-					nova.setObjeto(novoObjeto);
-					nova.setProximo(anterior.getProximo());
-					anterior.setProximo(nova);
-				}
-					
+			while(atual != null && ((Comparable) novoObjeto).compareTo(atual.getObjeto()) >= 0) {
+				anterior = atual;
+				atual = atual.getProximo();
 			}
-		}	
-	}
+			if(atual==lista.primeiro)
+				lista.inserirInicio(novoObjeto);
+			else {
+				Celula nova = new Celula();
+				nova.setObjeto(novoObjeto);
+				nova.setProximo(anterior.getProximo());
+				anterior.setProximo(nova);
+			}
+		}
+	}	
 
-	@Override
-	public void inserirFinal(Object o) {
-		this.lista.inserirFinal(o);
-	}
 
-	@Override
-	public Object removerInicio() {	
-		return (Celula)lista.removerInicio();
-	}
+@Override
+public void inserirFinal(Object o) {
+	this.lista.inserirFinal(o);
+}
 
-	@Override
-	public Celula recuperarInicio() {
-		return lista.primeiro;
-		
-	}
+@Override
+public Object removerInicio() {	
+	return (Celula)lista.removerInicio();
+}
 
-	@Override
-	public Iterador iterador() {
-		MIterador iterador= new MIterador(recuperarInicio());
-		return iterador;
-	}
+@Override
+public Celula recuperarInicio() {
+	return lista.primeiro;
 
-	@Override
-	public boolean estaVazia() {
-		return this.lista.estaVazia();
-	}
+}
+
+@Override
+public Iterador iterador() {
+	MIterador iterador= new MIterador(recuperarInicio());
+	return iterador;
+}
+
+@Override
+public boolean estaVazia() {
+	return this.lista.estaVazia();
+}
 }
