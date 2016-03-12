14fc8be672215c33802492377a1c4fcecc629d6c
MarcusAldrey <marcusaldrey@gmail.com>
2016-03-12 20:49:49 -0300

    Adicionado método TestInserirComPrioridadeSucesso() a classe FilaTest

--- WINMonster/src/unit/test/java/br/uefs/WinMonster/util/FilaTest.java
+++ WINMonster/src/unit/test/java/br/uefs/WinMonster/util/FilaTest.java
@@ -1,5 +1,29 @@
 package br.uefs.WinMonster.util;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 
 public class FilaTest {
-
+	
+	@Test
+	public void testInserirComPrioridadeSucesso() {
+		String nome1 = "João";
+		String nome2 = "Maria";
+		String nome3 = "Zuleica";
+		String nome4 = "Lokisley";
+		String nome5 = "Ana";
+		Fila fila = new Fila();
+		fila.inserirComPrioridade(nome1);
+		fila.inserirComPrioridade(nome2);
+		fila.inserirComPrioridade(nome3);
+		fila.inserirComPrioridade(nome4);
+		fila.inserirComPrioridade(nome5);
+		Iterador iterador = fila.iterador();
+		assertEquals("Ana",iterador.obterProximo());
+		assertEquals("João",iterador.obterProximo());
+		assertEquals("Lokisley",iterador.obterProximo());
+		assertEquals("Maria",iterador.obterProximo());
+		assertEquals("Zuleica",iterador.obterProximo());
+	}
 }
