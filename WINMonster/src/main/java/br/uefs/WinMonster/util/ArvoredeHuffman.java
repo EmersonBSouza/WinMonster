2949a728282f157aa26173c03c8a3527fa5d8ea9
EmersonBSouza <emerson_bsouza@outlook.com>
2016-03-12 16:05:32 -0300

    NoArvore

---
+++ WINMonster/src/main/java/br/uefs/WinMonster/util/ArvoredeHuffman.java
@@ -0,0 +1,7 @@
+package br.uefs.WinMonster.util;
+
+public class ArvoredeHuffman {
+
+	Fila arvores;
+	
+}
---
+++ WINMonster/src/main/java/br/uefs/WinMonster/util/NoArvore.java
@@ -0,0 +1,9 @@
+package br.uefs.WinMonster.util;
+
+public class NoArvore {
+
+	char letra;
+	int frequencia;
+	NoArvore filhoEsq;
+	NoArvore filhoDir;
+}
