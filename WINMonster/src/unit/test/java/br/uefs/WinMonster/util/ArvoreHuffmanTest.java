d315fef2cd902477841c68f9ac62174db377a4fb
MarcusAldrey <marcusaldrey@gmail.com>
2016-03-13 17:05:30 -0300

    Edições de comentário

--- WINMonster/src/unit/test/java/br/uefs/WinMonster/util/ArvoreHuffmanTest.java
+++ WINMonster/src/unit/test/java/br/uefs/WinMonster/util/ArvoreHuffmanTest.java
@@ -12,8 +12,8 @@ public class ArvoreHuffmanTest {
 
 		Fila fila = new Fila();
 
-		/* Os nós criados são inseridos na ordem que se espera que eles estejam
-		   localizados em uma árvore de HuffMan em uma leitura da esquerda para direita	*/
+		/* Criamos um vetor e nós com os caracteres da frase e suas frequências.
+		 * Os nós criados são inseridos no vetor em ordem crescente de frequência*/
 		NoArvore[] vetorAux = new NoArvore[9];
 
 		NoArvore no1 = new NoArvore();
@@ -61,19 +61,20 @@ public class ArvoreHuffmanTest {
 		no9.setFrequencia(6);
 		vetorAux[8] = no9;
 
-		/*Cria um objeto árvore para cada um dos nós, tendo eles como raiz, os caracteres de 
-		  menor frequência possuem preferência */
+		/*Cria um objeto ArvoreHuffman para cada um dos nós, tendo eles como raiz, e os insere 
+		 *em uma fila. Os caracteres de menor frequência possuem preferência. */
 		for(int i=0;i<vetorAux.length;i++) {
 			ArvoreHuffman arvore = new ArvoreHuffman();
 			arvore.setRaiz(vetorAux[i]);
 			fila.inserirFinal(arvore);;
 		}
 
+		//Cria-se uma árvore de Huffman com a fila gerada
 		ArvoreHuffman arvoreFinal = new ArvoreHuffman();
 		arvoreFinal = arvoreFinal.criaArvore(fila);
 
-		/*Código esperado que seja gerado por uma árvore de HuffMan para a String em questão
-		 * lida da esquerda para direita*/
+		/* Código esperado que seja gerado por uma árvore de HuffMan para a String em questão
+		 * em ordem crescente de frequência */
 		String codificacao = "01110" //\n 
 				+ "01111" //U
 				+ "0110" //T
@@ -89,14 +90,15 @@ public class ArvoreHuffmanTest {
 		//Vetor que armazenará os caracteres obtidos da árvore criada
 		char[] vetorObtido = new char[9];
 
-		/* Percorre a árvore utilizando a codificacao, onde 0 significa esquerda e 1 direita*/
+		/* Percorre a árvore lendo a codificacao, onde 0 significa esquerda e 1 direita*/
 		char direcao;
-		int i = 0;
-
-		int posicaoNoVetorObtido = 0;
+		int i = 0; //variável que controlará o andamento na String de codificação
+		int posicaoNoVetorObtido = 0; //variável que controlará o salvamento de letras encontradas no vetor
 		NoArvore atual;
 		atual = arvoreFinal.getRaiz();
 		do {
+			/* Caso o nó atual não possua letra, avança-se um nó na árvore, para a esquerda
+			 * ou para direita, dependendo do caractere lido na String de código*/
 			if(atual.getLetra() == '\0') {
 				direcao = vetorCodificacao[i];
 				if(direcao == '0')
@@ -104,7 +106,9 @@ public class ArvoreHuffmanTest {
 				else if (direcao == '1')
 					atual = atual.filhoDir;
 				i++;
-			} 
+			}
+			/* Caso seja encontrada uma letra(folha), ela é salva em um vetor e voltamos ao 
+			 * início da árvore, até que este esteja completamente preenchido*/
 			else {
 				vetorObtido[posicaoNoVetorObtido] = atual.getLetra();
 				posicaoNoVetorObtido++;
