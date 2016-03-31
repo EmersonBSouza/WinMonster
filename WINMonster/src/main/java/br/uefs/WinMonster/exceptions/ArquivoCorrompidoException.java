13f7ecc2b675409625d4e0a9644840fa464d8124
MarcusAldrey <marcusaldrey@gmail.com>
2016-03-30 21:41:20 -0300

    Criadas exceptions de usuário

---
+++ WINMonster/src/main/java/br/uefs/WinMonster/exceptions/ArquivoCorrompidoException.java
@@ -0,0 +1,9 @@
+package br.uefs.WinMonster.exceptions;
+
+public class ArquivoCorrompidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
---
+++ WINMonster/src/main/java/br/uefs/WinMonster/exceptions/FormatoArquivoInvalidoExceptions.java
@@ -0,0 +1,10 @@
+package br.uefs.WinMonster.exceptions;
+
+public class FormatoArquivoInvalidoExceptions extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
