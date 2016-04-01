5f7e32771e53f0126060cac9da354a767d90c2c5
MarcusAldrey <marcusaldrey@gmail.com>
2016-04-01 00:20:44 -0300

    Rascunho Bits modificado - agora usa ByteArrayOutPutSteam

--- WINMonster/src/unit/test/java/br/uefs/WinMonster/controller/RascunhoBits.java
+++ WINMonster/src/unit/test/java/br/uefs/WinMonster/controller/RascunhoBits.java
@@ -4,29 +4,29 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.BitSet;
 
 public class RascunhoBits {
 
 public static void main(String[] args) throws IOException {
 		
 		
-		BitSet b = new BitSet(16);
-		String n = //"10011111"
-					//+"01101111"
-					//+"00100101"
-					//+"11010001"
-					//+"10011010"
-					//"00111101"
-					//"01011100"
-					//+"1110010";
-					"0001000100010001";
+		boolean[] b = new boolean[80];
+		String n = "10011111"
+					+"01101111"
+					+"00100101"
+					+"11010001"
+					+"10011010"
+					+"00111101"
+					+"01011100"
+					+"1110010"
+					+"0001000100010001";
 		//1011 significa 11
 		//1111 significa 15
 		//Vem false por padrão
@@ -61,21 +61,26 @@ public static void main(String[] args) throws IOException {
 		//00001111
 		for(int i = 0; i < n.length();i++){
 			if(n.charAt(i) == '1'){
-				b.set(i,true);
+				b[i] = true;
 			}
 			else{
-				b.set(i,false);
+				b[i] = false;
 			}
 		}
 		int x=0;
 		
 		StringBuffer numero = new StringBuffer();
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
 		
-		while(x<16){
-			if(b.get(x)==true)
+		while(x<b.length){
+			if(b[x]==true) {
+				bytes.write(1);
 				numero.append(1);
-			else
+			}
+			else {
+				bytes.write(0);
 				numero.append(0);
+			}
 			x++;
 		}
 		
@@ -110,10 +115,8 @@ public static void main(String[] args) throws IOException {
 			FileOutputStream escrever = new FileOutputStream(arquivo);
 			
 			BufferedOutputStream bufferEscrever = new BufferedOutputStream(escrever);
-
-			String dina = b.toString();
-			bufferEscrever.write(b.toByteArray());
-			
+			//String dina = b.toString();
+			bytes.writeTo(bufferEscrever);
 			bufferEscrever.close();
 			escrever.close();
 		}catch(IOException e){
